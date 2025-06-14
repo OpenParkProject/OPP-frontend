a55c4721fde611a0ba599d9e7b4882aedc416027
LivioDad <livio.dad@gmail.com>
2025-06-14 18:32:09 +0200

    installation fixes

--- lib/config.dart
+++ lib/config.dart
@@ -4,3 +4,4 @@ const readerWsUrl = 'ws://localhost:10010';
 const String disableTotemOtp = "DISABLE-TOTEM";
 const totemTestOtpRfidOn = "TEST_OTP_RFID_ON";
 const totemTestOtpRfidOff = "TEST_OTP_RFID_OFF";
+const String totemSecret = "0";
